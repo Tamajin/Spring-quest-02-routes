cef7ecf8ecc86cb0dfae0238a8099f286f0d962c
Tamajin <wild.jlq@gmail.com>
2022-11-14 22:00:22 +0100

    challenge 2 done

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -1,17 +1,26 @@
 package com.wildcodeschool.doctor.controller;
 
 import com.wildcodeschool.doctor.model.Doctor;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 public class DoctorController {
 
-    @GetMapping("/doctor/")
+    @GetMapping("/doctor/{number}")
     @ResponseBody
-    public Doctor doctor() {
+    public Doctor doctor(@PathVariable int number) {
+        if (number == 13) {
+            return new Doctor(13, "Jodie Whittaker");
 
-        return null;
+        } else if (number >= 1 && number <= 12) {
+            throw new ResponseStatusException(HttpStatus.SEE_OTHER, "Content in progress");
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Can't resolve incarnation " + number);
     }
 }
